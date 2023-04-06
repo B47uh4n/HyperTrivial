354e5fee48778da0bd95c584739cbef7fb348ece
B47uh4n <105128740+B47uh4n@users.noreply.github.com>
2023-04-06 20:43:06 +0300

    v6.4-Level 1 Prototipi
    Sahne geçişleri için scenemanagement class yazmaya başladım şuan sadece level 1den 2ye geçerken kullandım.

--- HyperTrivial/Assets/Scripts/Player/PlayerController.cs
+++ HyperTrivial/Assets/Scripts/Player/PlayerController.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameOverMenu GameOverMenu;
+    public SceneManagement SceneManager;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("FalseAnswer"))
         {
             GameOverMenu.GameOver();
         }
+        else if (other.CompareTag("NextScene"))
+        {
+            SceneManager.NextScene();
+        }
     }
 
-    
+
+
 }
